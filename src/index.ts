376a002d2dcc920238cdd81c25eeedc3fd46a67d
Walter Gonzalez <walterglezp@gmail.com>
2025-05-29 16:34:04 -0400

    Add Column and Flex UI components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./components/Button/Button";
 export { default as Container } from "./components/layout/Container";
 export { default as Row } from "./components/layout/Row";
+export { default as Column } from "./components/layout/Column";
+export { default as Flex } from "./components/layout/Flex";
