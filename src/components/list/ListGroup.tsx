463f3c222a89a8e19b4f99a64cd38d6c38f7a7f2
Walter Gonzalez <walterglezp@gmail.com>
2025-05-29 16:47:04 -0400

    Add ListGroup and ListGroupItem UI components

---
+++ src/components/list/ListGroup.tsx
@@ -0,0 +1,37 @@
+import React, { FC, HTMLAttributes } from "react";
+import { generateClassnames } from "../../utils/classnames-helper";
+
+export interface ListGroupProps extends HTMLAttributes<HTMLUListElement> {
+  variant?: "basic" | "flush";
+  numbered?: boolean;
+  horizontal?: boolean;
+  equalWidth?: boolean;
+  className?: string;
+}
+
+const ListGroup: FC<ListGroupProps> = ({
+  className = "",
+  children,
+  variant = "basic",
+  numbered = false,
+  horizontal = false,
+  equalWidth = false,
+  ...rest
+}) => {
+  const classnames = generateClassnames({
+    "list-group": true,
+    "list-group-flush": variant === "flush",
+    "list-group-numbered": numbered,
+    "list-group-horizontal": horizontal,
+    "flex-fill": equalWidth,
+    [className]: !!className,
+  });
+
+  return (
+    <ul className={classnames} {...rest}>
+      {children}
+    </ul>
+  );
+};
+
+export default ListGroup;
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ export { default as Container } from "./components/layout/Container";
 export { default as Row } from "./components/layout/Row";
 export { default as Column } from "./components/layout/Column";
 export { default as Flex } from "./components/layout/Flex";
+export { default as ListGroup } from "./components/list/ListGroup";
+export { default as ListGroupItem } from "./components/list/ListGroupItem";
+export { generateClassnames } from "./utils/classnames-helper";
