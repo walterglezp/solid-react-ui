32b3859b11d510809c62206b6b5a3aace0e1e0b0
Walter Gonzalez <walterglezp@gmail.com>
2025-05-29 14:48:59 -0400

    Add typescript config

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -9,12 +9,10 @@ export const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
   return (
     <button
       {...props}
-      onClick={(e) => {
-        alert("Button clicked!");
-      }}
       style={{ padding: "8px 16px", fontSize: "16px", cursor: "pointer" }}
     >
       {children}
     </button>
   );
 };
+export default Button;
